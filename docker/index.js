956c3d2a50d21e5231439a095fbe9a54a5a72e2d
shuaibkhandev <skhan.csit@gmail.com>
2026-08-08 20:00:52 +0500

    first project deploy on docker

---
+++ docker/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to Node.js",
+    data: {
+      id: 1,
+      name: "Shuaib",
+      profession: "Software Engineer"
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
